62ae17116598fea7aae7ebea476e9d3685e9473f
hAdi <seersol92@users.noreply.github.com>
2024-08-02 03:41:43 +0500

    deployment issues fixed

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,7 +13,9 @@ import { Navigation } from "swiper/modules";
 import "swiper/css"; // Import Swiper styles
 import "swiper/css/navigation"; // Import Swiper navigation styles
 import Image from "next/image";
+import { Swiper as SwiperCore } from 'swiper';
 
+// Project data
 const projects = [
   {
     id: "01",
@@ -129,13 +131,13 @@ const projects = [
 
 export default function Page() {
   const [project, setProject] = useState(projects[0]);
-  const [swiper, setSwiper] = useState(null);
+  const [swiper, setSwiper] = useState<SwiperCore | null>(null);
 
-  const handleChangeSlide = (swiper: any) => {
-    setProject(projects[swiper.activeIndex]);
-    if (swiper) {
-      const isBeginning = swiper.isBeginning;
-      const isEnd = swiper.isEnd;
+  const handleChangeSlide = (swiperInstance: SwiperCore) => {
+    setProject(projects[swiperInstance.activeIndex]);
+    if (swiperInstance) {
+      const isBeginning = swiperInstance.isBeginning;
+      const isEnd = swiperInstance.isEnd;
       setDisableButtons({
         prev: isBeginning,
         next: isEnd,
@@ -183,7 +185,7 @@ export default function Page() {
 
           <div className="flex gap-5">
             <Link
-              href="#"
+              href={project.live}
               title="Live Project"
               className="flex justify-center items-center bg-white/5 rounded-full w-[68px] h-[68px]"
             >
@@ -191,7 +193,7 @@ export default function Page() {
             </Link>
 
             <Link
-              href="#"
+              href={project.github}
               title="Github repository"
               className="flex justify-center items-center bg-white/5 rounded-full w-[68px] h-[68px]"
             >
@@ -205,7 +207,7 @@ export default function Page() {
             spaceBetween={30}
             slidesPerView={1}
             modules={[Navigation]}
-            onSlideChange={handleChangeSlide}
+            onSlideChange={({swiper}) => handleChangeSlide(swiper)}
             className="h-[520px] mb-12"
             navigation={{
               nextEl: ".swiper-button-next",
@@ -221,7 +223,7 @@ export default function Page() {
                 <Image
                   src={project.image}
                   fill
-                  alt=""
+                  alt={project.title}
                   className=" object-cover"
                 />{" "}
                 {/* Replace with actual image or content */}
